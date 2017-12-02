899691297e6f2bcb5c7375e188aaf5b5b9335827
suchamillennial <tejgidvani@hotmail.com>
2017-12-02 02:43:27 -0800

    Edited the stateMain file.
    Loaded a background and added a background image
    
    Placed the background on the stage

--- js/stateMain.js
+++ js/stateMain.js
@@ -1,18 +1,23 @@
 var StateMain={    
     
-   preload:function()
+    //This function is used for loading images sounds and other game library stuff
+    preload:function()
     {
-       
+        //load the background image 
+        game.load.image("background","images/background1.png");
+
+        console.log("Preload done!");
     },
     
+    //Where everything is initialized and set up 
     create:function()
     {
-        
+        //place the background image at (0,0)
+        this.char1=game.add.sprite(0,0,"background");
     },
-    
+    //Main game loop
     update:function()
     {       
         
-    }    
-    
+    }
 }
