695d7c568cf43c8dd8983766a1e4c30103c05326
suchamillennial <tejgidvani@hotmail.com>
2017-12-02 03:40:42 -0800

    Change the variable name for the background
    Added the spartan character and changed some properties for it
    
    Made the spartan spin the gameloop.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ window.onload = function()
     //Create a game of 480 X 640 Dimensions and auto choose the graphics library supported
     game=new Phaser.Game(480,640,Phaser.AUTO,"ph_game");
     
+    game.global ={
+        angle : 0
+    }
     //Add the game state "StateMain" from that file
     game.state.add("StateMain",StateMain);
     //Start the game with game state "StateMain"
--- js/stateMain.js
+++ js/stateMain.js
@@ -5,7 +5,7 @@ var StateMain={
     {
         //load the background image 
         game.load.image("background","images/background1.png");
-
+        game.load.image("spartan","images/spartan.png");
         console.log("Preload done!");
     },
     
@@ -13,11 +13,24 @@ var StateMain={
     create:function()
     {
         //place the background image at (0,0)
-        this.char1=game.add.sprite(0,0,"background");
+        this.background = game.add.sprite(0,0,"background");
+        this.background.height = game.height;
+
+        this.spartan = game.add.sprite(100,100,"spartan");
+        this.spartan.height = .25*this.spartan.height;
+        this.spartan.width = .25*this.spartan.width;
+        this.spartan.alpha = 1;
+        //The anchor is now set to the center of the sprite so it spins around its center
+        this.spartan.anchor.setTo(0.5,0.5);
+        
+        
     },
     //Main game loop
     update:function()
     {       
-        
+        this.spartan.angle = this.spartan.angle + 30;
+        if(this.spartan.angle == 360){
+            this.spartan.angle=0;
+        }
     }
 }
