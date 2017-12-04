81c82df242baeff2a52f18e67915d1b4a36f4225
suchamillennial <tejgidvani@hotmail.com>
2017-12-04 10:35:19 -0800

    Replaced static spartan sprite with a sprite sheet that can be animated.

--- js/stateMain.js
+++ js/stateMain.js
@@ -5,7 +5,7 @@ var StateMain={
     {
         //load the background image 
         game.load.image("background","images/background1.png");
-        game.load.image("spartan","images/spartan.png");
+        game.load.spritesheet("spartan","images/sprite sheet man.png",40,40,3);
         console.log("Preload done!");
     },
     
@@ -16,9 +16,13 @@ var StateMain={
         this.background = game.add.sprite(0,0,"background");
         this.background.height = game.height;
 
-        this.spartan = game.add.sprite(100,100,"spartan");
-        this.spartan.height = .25*this.spartan.height;
-        this.spartan.width = .25*this.spartan.width;
+        this.spartan = game.add.sprite(100,100,"spartan")
+        this.spartan.frame = 1;
+        this.spartan.animations.add('idle',[0,1],6,true);
+        this.spartan.animations.play('idle');
+
+        this.spartan.scale.y = 2;
+        this.spartan.scale.x = 2;
         this.spartan.alpha = 1;
         //The anchor is now set to the center of the sprite so it spins around its center
         this.spartan.anchor.setTo(0.5,0.5);
@@ -28,9 +32,10 @@ var StateMain={
     //Main game loop
     update:function()
     {       
-        this.spartan.angle = this.spartan.angle + 30;
-        if(this.spartan.angle == 360){
-            this.spartan.angle=0;
-        }
+        // this.spartan.angle = this.spartan.angle + 30;
+        // if(this.spartan.angle == 360){
+        //     this.spartan.angle=0;
+        // }
+
     }
 }
